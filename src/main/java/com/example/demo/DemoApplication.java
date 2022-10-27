446a6a0d547cc23480b24c763bb8a1cb984edfc4
hsiaoleyu <111258171+hsiaoleyu@users.noreply.github.com>
2022-10-27 15:42:16 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello N!";
+        return "Hello Nanshanlife!";
     }
 
     public static void main(String[] args) {
